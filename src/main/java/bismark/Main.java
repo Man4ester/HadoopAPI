ae5c0e2df3448822cc4cbae875c7362ccef0569b
Man4ester <byrkovskiy@gmail.com>
2016-08-23 21:42:08 +0300

    init

---
+++ src/main/java/bismark/Main.java
@@ -0,0 +1,11 @@
+package main.java.bismark;
+
+/**
+ * Created by Bismark on 23.08.2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print("Hellow world");
+    }
+}
